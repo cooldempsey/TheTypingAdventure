4fb2d9c57b70406819f9f4425ea83b596c5d496b
dempseyfrancis11@yahoo.com <dempseyfrancis11@yahoo.com>
2025-02-22 12:15:16 -0600

    Created MainMenu implementation and basic mainMenu screen.

--- TheTypingAdventure/DisplayScreen.cs
+++ TheTypingAdventure/DisplayScreen.cs
@@ -6,7 +6,44 @@ using System.Threading.Tasks;
 
 namespace TypingAdventureProgram
 {
-    class DisplayScreen
+    static class DisplayScreen
     {
+        public static void MainMenu(int levelSelection)
+        {
+            newScreen();
+            Console.WriteLine("Choose your level:");
+            for (int i = 0; i < MainProgram.NUM_LEVELS; i++)
+            {
+                if (i == levelSelection)
+                {
+                    Console.Write(">");
+                }
+                Console.Write($"Level {i}  ");
+            }
+        }
+
+        public static void DisplayLevel(string challengeText, string userText, float remainingTime)
+        {
+            
+        }
+
+        public static void LoseScreen()
+        {
+
+        }
+
+        public static void WinScreen()
+        {
+
+        }
+
+        private static void PressKeyToContinue()
+        {
+            Console.Write("Press any key to continue . . .");
+            Console.ReadKey(true);
+        }
+
+        private static void newScreen()
+            => Console.WriteLine("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
     }
 }
